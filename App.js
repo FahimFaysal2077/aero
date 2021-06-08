fa754d08c6fe0359fd9ed092cdbc437bab7f0c91
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-06-08 20:03:21 +0600

    header section add

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Screen name = 'Login' component = {LoginScreen} />
       <View style={styles.container}>
         <StatusBar style="auto" />
 
