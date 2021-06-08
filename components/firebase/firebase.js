1b5ff01df6e5a87c063f1d66b614599320eb5bc6
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-06-08 16:46:34 +0600

    first commit

---
+++ components/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiJ1olccG_HnVyRWlbiJCHp3mXOdRXtkw",
+    authDomain: "aero-fahim.firebaseapp.com",
+    projectId: "aero-fahim",
+    storageBucket: "aero-fahim.appspot.com",
+    messagingSenderId: "880683034948",
+    appId: "1:880683034948:web:9d257e83c5736af1948f11"
+  };
