3e483b8f87c2a93ea2de64829121fbe0744cc973
fahimfaysal2077 <fahim.faysal2077@gmail.com>
2021-06-17 13:00:47 +0600

    chat romm color change & add video call button

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import RegisterScreen from './components/Screens/RegisterScreen/RegisterScreen';
 import HomeScreen from './components/Screens/HomeScreen/HomeScreen';
 import AddChatScreen from './components/Screens/AddChatScreen/AddChatScreen';
+import ChatScreen from './components/Screens/ChatScreen/ChatScreen';
 
 
 const Stack = createStackNavigator();
@@ -25,6 +26,7 @@ export default function App() {
         <Stack.Screen name='Register' component={RegisterScreen} />
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='AddChat' component={AddChatScreen} />
+        <Stack.Screen name='Chat' component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 
--- components/CustomListItem/CustomListItem.js
+++ components/CustomListItem/CustomListItem.js
@@ -4,7 +4,7 @@ import { ListItem, Avatar } from 'react-native-elements';
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
     return (
-        <ListItem>
+        <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider >
             <Avatar 
             source={{
                 uri:'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
@@ -12,7 +12,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
             />
             <ListItem.Content>
                 <ListItem.Title style={{ fontWeight: '800' }}>
-                    Google Chat
+                    {chatName}
                 </ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
                     This is a test subtitle
